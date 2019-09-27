4b61fcce3a3d7f6fba869a105db247b6d31517fe
Shubham <unwiredshubham@gmail.com>
2019-09-27 20:46:17 +0530

    Updated settings

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,6 +1,6 @@
+import django-heroku
 import os
 import environ
-import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
